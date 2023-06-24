7786174a7432d4c9e616accb94b1986d13178425
Andrey <awesome.foxe@yandex.ru>
2023-06-24 21:13:43 +0300

    записан метод сравнения длинны индексов первого массива и запись верных ответов во второй массив

--- Program.cs
+++ Program.cs
@@ -2,3 +2,11 @@
 string[] FirstArray = Console.ReadLine().Split();
 string[] SecondArray = new string[FirstArray.Length];
 int j = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[j] = FirstArray[i];
+            j++;
+        }
+    }
